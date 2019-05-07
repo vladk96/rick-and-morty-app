c01c93bac0b9af4595ae72c5a475fdb7823fd9a2
vladk96 <vladk961@gmail.com>
2019-05-07 10:45:41 +0300

    Updates

---
+++ precache-manifest.a69a8945671d897bc05a0a5179f1dd66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d038f2afd32dcc50df80d143e0d3fd4e",
+    "url": "/rick-and-morty-app/static/media/spinner.d038f2af.svg"
+  },
+  {
+    "revision": "825488f7859acb3fac46bae10607618a",
+    "url": "/rick-and-morty-app/static/media/rickandmorty2.825488f7.png"
+  },
+  {
+    "revision": "9021495090674356c90c",
+    "url": "/rick-and-morty-app/static/js/runtime~main.f58b98ef.js"
+  },
+  {
+    "revision": "a8e0df6a6b6d6e01c20b",
+    "url": "/rick-and-morty-app/static/js/main.fc4b48b2.chunk.js"
+  },
+  {
+    "revision": "0c0b4ab511e3e8759bfb",
+    "url": "/rick-and-morty-app/static/js/2.13fc64f3.chunk.js"
+  },
+  {
+    "revision": "a8e0df6a6b6d6e01c20b",
+    "url": "/rick-and-morty-app/static/css/main.1f88641f.chunk.css"
+  },
+  {
+    "revision": "4753b6c094034be7a0c539714e170ed7",
+    "url": "/rick-and-morty-app/index.html"
+  }
+];
